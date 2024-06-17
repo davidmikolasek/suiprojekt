1834c885ec3d7b3f403b77151835ef8a4106922f
davidmikolasek <94286663+davidmikolasek@users.noreply.github.com>
2024-06-17 20:36:46 +0200

    Add files via upload

---
+++ digit rec logic/digitrec.py
@@ -0,0 +1,41 @@
+import os
+
+import numpy as np
+import tensorflow as tf
+
+
+print("Welcome to the NeuralNine (c) Handwritten Digits Recognition v0.1")
+
+# Decide if to load an existing model or to train a new one
+train_new_model = True
+    # Loading the MNIST data set with samples and splitting it
+mnist = tf.keras.datasets.mnist
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+    # Normalizing the data (making length = 1)
+X_train = tf.keras.utils.normalize(X_train, axis=1)
+X_test = tf.keras.utils.normalize(X_test, axis=1)
+
+    # Create a neural network model
+    # Add one flattened input layer for the pixels
+    # Add two dense hidden layers
+    # Add one dense output layer for the 10 digits
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
+
+    # Compiling and optimizing model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # Training the model
+model.fit(X_train, y_train, epochs=3)
+
+    # Evaluating the model
+val_loss, val_acc = model.evaluate(X_test, y_test)
+print(val_loss)
+print(val_acc)
+
+    # Saving the model
+model.save('handwritten_digits.keras')
